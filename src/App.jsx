67f6004768506d0c0fad5f9b81a7c3724b149a30
Bharti Patel <patelshree40@gmail.com>
2024-08-25 12:55:12 +0530

    project initialized

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>Hello World! Long time no see!</div>
+    </>
+  )
+}
+
+export default App
